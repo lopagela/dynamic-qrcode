026fc998ed2ef543503a2105ded35ceeaecf5314
Louis Melchior <louis@jaris.io>
2022-04-17 20:08:20 +0200

    Configure the logging before importing the code for initialization logs to work

--- dynamic_qrcode/__main__.py
+++ dynamic_qrcode/__main__.py
@@ -1,11 +1,11 @@
 import logging
 
-from dynamic_qrcode.app import app
-
-
 PRETTY_LOG_FORMAT = '%(asctime)s.%(msecs)03d [%(levelname)s] %(name)s - %(message)s'
 logging.basicConfig(level="DEBUG", format=PRETTY_LOG_FORMAT, datefmt="%H:%M:%S")
 
 log = logging.getLogger(__name__)
+
+# Configuring the logging before importing the source code
+from dynamic_qrcode.app import app
 
 log.info("Application loaded")
